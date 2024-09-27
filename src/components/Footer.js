b7efee0014d2f91dc7bf495f972ecc121e64c9c1
Rakesh Kumar <rakesh.kumar@techenhance.com>
2024-09-27 13:35:28 +0530

    updated new logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
-import "./Header.css"
+import "./Header.css";
 import {
   faFacebook,
   faTwitter,
@@ -10,18 +10,16 @@ import {
 } from "@fortawesome/free-brands-svg-icons";
 import { faLinkedin, faWhatsapp } from "@fortawesome/free-brands-svg-icons"; // Import LinkedIn and WhatsApp icons
 
-import "./footer.css"
+import "./footer.css";
 import { Element } from "react-scroll";
 import { Link, scroller } from "react-scroll";
 import MyVerticallyCenteredModal from "./Modals";
 import EmailModal from "./EmailModal";
-
-
 const Footer = () => {
   const [showModal, setShowModal] = useState(false);
-   const [show, setShow] = useState(false);
+  const [show, setShow] = useState(false);
 
-   const handleClose = () => setShow(false);
+  const handleClose = () => setShow(false);
 
   const handleSubmit = () => {
     // Perform any necessary submission logic here
@@ -39,9 +37,9 @@ const Footer = () => {
       delay: 0,
       // smooth: "easeInOutQuint",
     });
-  }
+  };
   return (
-    <Element name="footer" >
+    <Element name="footer">
       {" "}
       <Container fluid className="footercss p-5 section">
         <main>
@@ -50,9 +48,9 @@ const Footer = () => {
             <Col xs={12} md={3}>
               <img
                 fluid
-                src="./logodark.png"
+                src="./mypickup_logo_light.png"
                 alt="Logo"
-           className="nav-logo"
+                className="nav-logo"
               />
             </Col>
 
@@ -61,12 +59,13 @@ const Footer = () => {
               <ul className="quick-links" style={{ listStyle: "none" }}>
                 <li>
                   {" "}
-                  <Link className="faq"
+                  <Link
+                    className="faq"
                     to="faq"
-
                     onClick={() => {
-                        window.location.href = 'https://docs.google.com/document/d/1jnT5vMYT4p5E-lPqmDuCTTMKCaz8DaUgrB-gaucsKMQ/edit#heading=h.2m5g35pq7759';
-                        return null;
+                      window.location.href =
+                        "https://docs.google.com/document/d/1jnT5vMYT4p5E-lPqmDuCTTMKCaz8DaUgrB-gaucsKMQ/edit#heading=h.2m5g35pq7759";
+                      return null;
                     }}
                   >
                     Terms & Conditions
@@ -77,14 +76,12 @@ const Footer = () => {
                   <Link
                     to="features"
                     className="service"
-                    
                     onClick={() => scrollToSection("features")}
                   >
                     Our Services
                   </Link>
                   <Link
                     to="number"
-                  
                     style={{
                       fontSize: "18px",
                       fontWeight: "500",
@@ -93,9 +90,7 @@ const Footer = () => {
                       padding: "10px",
                     }}
                     onClick={() => scrollToSection("number")}
-                  >
-                
-                  </Link>
+                  ></Link>
                 </li>
               </ul>
             </Col>
@@ -193,59 +188,48 @@ const Footer = () => {
                   >
                     <FontAwesomeIcon icon={faWhatsapp} /> {/* WhatsApp icon */}
                   </a>
-                  </li>
+                </li>
                 <Link
-                    to="number"
-                    style={{
-                      fontSize: "18px",
-                      fontWeight: "500",
-                      color: "white",
-                      cursor: "pointer",
-
-                    }}
-                    onClick={() => scrollToSection("number")}
-                  >
-                    
-                  </Link>
-                  
+                  to="number"
+                  style={{
+                    fontSize: "18px",
+                    fontWeight: "500",
+                    color: "white",
+                    cursor: "pointer",
+                  }}
+                  onClick={() => scrollToSection("number")}
+                ></Link>
               </ul>
-              
-              <ul>
-              
-              
-              <a
-  href="tel:+9188677122880"
-  target="blank"
-  rel="noopener noreferrer"
-  style={{
-    fontSize: "20px",
-    padding: "5px",
-    color: "white",
-    textDecoration: "none", // Add this line to remove the underline
-  }}
->
-  <FontAwesomeIcon icon={faPhone} /> {/* Call icon */}
-</a>
 
-                
-              
-                  
-                  <Link
-                    to="features"
-                    style={{
-                      fontSize: "18px",
-                      fontWeight: "500",
-                      color: "white",
-                      cursor: "pointer",
-                      padding: "1px",
-                    }}
-                    onClick={() => scrollToSection("features")}
-                  >
-                        +91 8867712288 
-                    
-                  </Link>
-                  
-                </ul>
+              <ul>
+                <a
+                  href="tel:+9188677122880"
+                  target="blank"
+                  rel="noopener noreferrer"
+                  style={{
+                    fontSize: "20px",
+                    padding: "5px",
+                    color: "white",
+                    textDecoration: "none", // Add this line to remove the underline
+                  }}
+                >
+                  <FontAwesomeIcon icon={faPhone} /> {/* Call icon */}
+                </a>
+
+                <Link
+                  to="features"
+                  style={{
+                    fontSize: "18px",
+                    fontWeight: "500",
+                    color: "white",
+                    cursor: "pointer",
+                    padding: "1px",
+                  }}
+                  onClick={() => scrollToSection("features")}
+                >
+                  +91 8867712288
+                </Link>
+              </ul>
             </Col>
           </Row>
         </main>
