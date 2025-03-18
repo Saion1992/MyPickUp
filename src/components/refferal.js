a54e151bd5636f408e60e72f7808c278673495d3
Rakesh Kumar <rakesh@mypickup.in>
2025-03-19 00:58:32 +0530

    change referral_code deeplink

--- src/components/refferal.js
+++ src/components/refferal.js
@@ -1,38 +1,25 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const ReferralRedirect = () => {
   const navigate = useNavigate();
+  const [redirectLink, setRedirectLink] = useState("");
 
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
-    const userId = urlParams.get("ref");
+    const userId = urlParams.get("referral_code");
 
     if (userId) {
-      const appLink = `mypickup://refer?ref=${userId}`;
-      const playStoreLink =
-        "https://play.google.com/store/apps/details?id=in.mypickup.mypickup";
-      const appStoreLink =
-        "https://apps.apple.com/in/app/mypickup/id6737308804";
+      const playStoreLink = `https://play.google.com/store/apps/details?id=in.mypickup.mypickup&referrer=${userId}`;
+      const appStoreLink = `https://apps.apple.com/in/app/mypickup/id6737308804?referrer=${userId}`;
 
-      // Try opening the app
-      window.location.href = appLink;
-
-      setTimeout(() => {
-        if (document.hidden || document.webkitHidden) {
-          console.log("App opened successfully");
-        } else {
-          // App is not installed, redirect to the Play Store or App Store
-          console.log("window.location.href:", window.location.href);
-          if (/android/i.test(navigator.userAgent)) {
-            window.location.href = playStoreLink;
-            console.log("window.location.href2:", window.location.href);
-          } else if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
-            window.location.href = appStoreLink;
-            console.log("window.location.href3:", window.location.href);
-          }
-        }
-      }, 2500);
+      if (/android/i.test(navigator.userAgent)) {
+        setRedirectLink(playStoreLink);
+      } else if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
+        setRedirectLink(appStoreLink);
+      } else {
+        navigate("/");
+      }
     } else {
       navigate("/");
     }
@@ -41,6 +28,16 @@ const ReferralRedirect = () => {
   return (
     <div>
       <h2>Redirecting...</h2>
+      {redirectLink ? (
+        <a href={redirectLink}>
+          Click here if you are not redirected automatically.
+        </a>
+      ) : (
+        <p>
+          Invalid referral code or unsupported platform. Redirecting to the
+          homepage...
+        </p>
+      )}
     </div>
   );
 };
